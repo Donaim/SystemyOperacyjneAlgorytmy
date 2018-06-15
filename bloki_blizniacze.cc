bdc264cacc2d474da0e4f585477078fd81850950
Donaim <3Donaim@gmail.com>
2018-06-15 12:52:55 +0200

    bloki blizniacze almost works

--- bloki_blizniacze.cc
+++ bloki_blizniacze.cc
@@ -6,7 +6,7 @@ using namespace std;
 struct block {
     block(int tmem) : total_mem(tmem), used_mem(0), pid(-1), free(true) {}
     block(int tmem, int umem, int id) : total_mem(tmem), used_mem(umem), pid(id), free(false) {}
-    const int
+    int
         total_mem,
         used_mem,
         pid;
@@ -18,7 +18,7 @@ vector< block > mem;
 
 void show_mem() {
     for (auto & p : mem) {
-        cout << (p.free ? '0' : 'X') << ' ' << p.total_mem << endl;
+        cout << (p.free ? " " : to_string(p.pid)) << ' ' << p.total_mem << endl;
     }
     cout << endl;
 }
@@ -66,33 +66,47 @@ void create_process(int need, int id) {
 }
 bool merge_one_free() {
     for (int i = 0; i < mem.size(); i++) {
+        if (!mem[i].free) { continue; }
         int find = find_mem(mem[i].total_mem, i + 1);
         if (find != -1) {
             int new_size = mem[i].total_mem * 2;
-            mem.push_back(block{new_size});
 
             mem.erase(mem.begin() + i);
             mem.erase(mem.begin() + find);
-            i--;
+
+            mem.push_back(block{new_size});
             return true;
         }
     }
     return false;
 }
-void delete_process(int mem_index) {
+int find_pid(int pid) {
+    for (int i = 0; i < mem.size(); i++) {
+        if (mem[i].pid == pid) { return i; }
+    }
+    return -1;
+}
+void delete_process(int pid) {
+    int mem_index = find_pid(pid);
+    if (mem_index == -1) { throw invalid_argument{"wrong pid: " + to_string(pid)}; }
     mem[mem_index] = block{mem[mem_index].total_mem};
-    while(merge_one_free());
+    merge_one_free();
+    // while(merge_one_free());
 }
 
 int main()
 {
-    mem.push_back(make_pair(max_mem, true));
+    mem.push_back(block{max_mem});
     cout << "max_mem=" << max_mem << endl;
 
     int processes[] = {100, 240, 64, 250};
 
     for (int i = 0; i < sizeof(processes) / sizeof(int); i++) {
         create_process(processes[i], i);
+        show_mem();
+    }
+    for (int i = 0; i < 3; i++) {
+        delete_process(i);
         show_mem();
     }
 
